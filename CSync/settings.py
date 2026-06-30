59929bcc642af44bb7255475deed39b90988b57e
KAGWIRIAO <kagwiriafaith45@gmail.com>
2026-06-30 08:45:09 +0300

    Change error templates root directory

--- CSync/settings.py
+++ CSync/settings.py
@@ -97,9 +97,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATICFILES_DIRS = []
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
