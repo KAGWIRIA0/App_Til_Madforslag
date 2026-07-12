d46d51c757d8247db4a0415aea237b67dc244afc
KAGWIRIAO <kagwiriafaith45@gmail.com>
2026-07-12 18:34:50 +0300

    Change Storage of Files

--- CSync/settings.py
+++ CSync/settings.py
@@ -106,7 +106,6 @@ STATICFILES_DIRS = []
 # ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -128,7 +127,15 @@ CLOUDINARY_STORAGE = {
     'API_KEY':    os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage' 
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 handler404 = 'core.views.handler404'
 handler500 = 'core.views.handler500'
